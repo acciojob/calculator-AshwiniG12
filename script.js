7a52b26b795446c58f13b28e2fb71438c6163401
AshwiniG12 <121161369+AshwiniG12@users.noreply.github.com>
2023-02-14 11:23:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your code here
+const input = document.getElementById("input");
+const buttons = document.querySelectorAll("button");
+buttons.forEach(button => {
+  button.addEventListener("click", function() {
+    if (this.id === "plus") {
+      input.value += "+";
+    } else if (this.id === "minus") {
+      input.value += "-";
+    } else if (this.id === "multiply") {
+      input.value += "*";
+    } else if (this.id === "divide") {
+      input.value += "/";
+    } else if (this.id === "ans") {
+      try {
+        input.value = eval(input.value);
+      } catch (error) {
+        input.value = error.message;
+      }
+    } else if (this.id === "clr") {
+      input.value = "";
+    } else {
+      input.value += this.textContent;
+    }
+  });
+});
+
